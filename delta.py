96feb1bf110efa303fafbdb4dd747beca3923302
Dayvid Santana <110176533+kamatashi@users.noreply.github.com>
2025-02-01 23:32:52 -0300

    Create delta.py

---
+++ delta.py
@@ -0,0 +1,12 @@
+"""
+Elabore uma função (chame de delta(a,b,c))
+em Python para calcular o discriminante de
+uma equação do segundo grau, do tipo ax2 +
+bx + c = 0.
+"""
+
+def delta(a, b, c):
+    return b**2 - 4*a*c
+
+
+print(delta(2,4,7))
