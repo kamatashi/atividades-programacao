5abe3ebed8f632e6c2d0c782be7044648fad5205
Dayvid Santana <110176533+kamatashi@users.noreply.github.com>
2025-02-01 23:54:25 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,15 @@
+'''
+Faça um programa principal em Python que calcula a
+média de quatro números reais introduzidos pelo
+usuário no teclado. 
+'''
+
+
+a = float(input('Digite o primeiro valor: '))
+b = float(input('Digite o segundo valor: '))
+c = float(input('Digite o terceiro valor: '))
+d = float(input('Digite o quarto valor: '))
+
+media = (a+b+c+d) / 4
+
+print(media)
