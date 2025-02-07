2232ff43ffd2ae8b0fc242c8208fe9e06ce442c4
Dayvid Santana <110176533+kamatashi@users.noreply.github.com>
2025-02-06 21:56:58 -0300

    Update desvioPadrao.py

--- desvioPadrao.py
+++ desvioPadrao.py
@@ -1,4 +1,19 @@
 def somatorio(n, func):
     if n == 0:
         return 0
-    return n + somatorio(n - 1)
+    func(n)
+    return n + somatorio(n - 1, func)
+
+
+
+def desvio():
+    return
+
+
+def addic(a,b=1):
+    return a*b
+
+
+resultado = somatorio(5,addic)
+
+print(resultado)
