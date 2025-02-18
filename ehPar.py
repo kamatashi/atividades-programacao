15773285b0ad38f8d3638b92bbb65d9ea34d3489
Dayvid Santana <110176533+kamatashi@users.noreply.github.com>
2025-02-18 07:26:03 -0300

    add ehPar.py

---
+++ ehPar.py
@@ -0,0 +1,10 @@
+number = int(input('Digite o número!')
+
+def ehPar(number):
+    if (number % 2) = 0:
+        print('O número é par')
+    else:
+        print('O número é impar')
+        
+  
+ehPar(number)
