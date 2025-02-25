190f4e68c9b3f187f4b7b5a682f9c4a0c1bcd892
Dayvid Santana <110176533+kamatashi@users.noreply.github.com>
2025-02-25 07:41:53 -0300

    Atualizar o

---
+++ maiorQue.py
@@ -0,0 +1,13 @@
+
+
+
+def maiorQ1(numero):
+    if numero > 200 or numero < 100:
+        print("ok")
+        
+
+def maiorQ2(numero):
+    if numero > 200:
+        print("ok")
+    if numero < 100:
+        print("ok)
