d921f5bd4a7609cd9fdbf4fd545f434fcd5639f6
Dayvid Santana <110176533+kamatashi@users.noreply.github.com>
2025-02-25 09:58:56 -0300

    Atualizar o perimetroTriangulo.py

--- perimetroTriangulo.py
+++ perimetroTriangulo.py
@@ -1,22 +1,20 @@
 import math
 
-
 area = 0
 perimetroTriangulo = 0
 
-def triangulo(a,b,c):
+def triangulo(a, b, c):
     global area
     global perimetroTriangulo
-    sPerimetro = (a+b+c)/2
+    sPerimetro = (a + b + c) / 2
     area = math.sqrt(sPerimetro * (sPerimetro - a) * (sPerimetro - b) * (sPerimetro - c))
     perimetroTriangulo = sPerimetro*2
-
 
 a = int(input('Qual o valor de a?: '))
 b = int(input('Qual o valor de b?: '))
 c = int(input('Qual o valor de c?: '))
 
-triangulo(a,b,c)
+triangulo(a, b, c)
 
-print('O perimetro é: ', perimetroTriangulo)
-print('A área dp triangulo é: ', area)
+print('O perímetro é:', perimetroTriangulo)
+print('A área do triângulo é:', area)
