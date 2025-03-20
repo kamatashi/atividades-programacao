52dd37e82de242e5af7bd53db245300ada8dac43
Dayvid Santana <110176533+kamatashi@users.noreply.github.com>
2025-03-20 11:46:03 -0300

    Create inversaoString.py

---
+++ inversaoString.py
@@ -0,0 +1,14 @@
+'''
+É possível inverter strings usando um intervalo
+negativo. Desenvolver uma função em
+Python que receba uma string e retorne a
+string invertida.
+Dica: Fica mais fácil usando slice
+Faça um programa principal para testar se a
+função acima está correta.
+'''
+
+
+text = input("Qual frase será invertida?\n")
+
+print(text[::-1])
